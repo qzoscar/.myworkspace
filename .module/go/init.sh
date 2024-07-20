308372e2aa672af69654e2293799cde96bf0088e
gugu <qzbrew@vip.qq.com>
2024-07-20 12:29:51 +0800

    fix

--- .module/go/init.sh
+++ .module/go/init.sh
@@ -2,8 +2,8 @@
 
 if [ -f "${HOME}/.g/env" ]; then
     . "${HOME}/.g/env"
+    export PATH="${HOME}/go/bin:$PATH"
+    export PATH="$PATH:$HOME/.local/bin" # protobuf
+    export PATH="$(go env GOPATH)/bin:$PATH"
 fi
 
-export PATH="${HOME}/go/bin:$PATH"
-export PATH="$PATH:$HOME/.local/bin" # protobuf
-export PATH="$(go env GOPATH)/bin:$PATH"
