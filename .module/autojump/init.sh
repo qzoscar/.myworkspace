bcea2c7550ab98863cbc53b0defaa3e70996115c
gugu <qzbrew@vip.qq.com>
2024-07-20 12:31:42 +0800

    fix ubuntu use autojump

--- .module/autojump/init.sh
+++ .module/autojump/init.sh
@@ -2,3 +2,5 @@
 
 # autojump
 [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
+
+[ -f /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh
